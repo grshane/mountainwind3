bbf33cd66e278464b5c51ca70590494f22f029c3
Gregg Shanefelt <gshanefelt@phase2technology.com>
2019-01-14 15:15:10 -0500

    Update homepage content

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,10 +8,24 @@ const IndexPage = () => (
     <HeroVideo />
     <h2>About Mountain Wind Productions</h2>
     <p>
-      Mountain Wind is a full-service production company specialized in content
-      development, film production, and entertainment risk control. With a
-      diverse, highly talented, and experienced team, we want to help you bring
-      your stories to life in a cinematic, compelling, and safe way.
+      With a commitment to first-class service with integrity, Mountain Wind is
+      a full-service production company specialized in content development,
+      production, and entertainment risk management. We offer high-quality video
+      production in addition to risk management services for studios, local
+      businesses, marketing agencies, producers, entrepreneurs, and more.
+    </p>
+    <p>
+      Whether you are an author or publisher seeking quality content to market
+      your most recent work, a non-profit looking to make a big impact, or an
+      experienced producer seeking a production team and/or risk management
+      services, we will stop at nothing to give you the highest quality service
+      available to help you maximize your ROI.
+    </p>
+    <p>
+      Safety should be a top priority, not a stressful worry for producers. We
+      take a proactive, strategic approach to risk management in production,
+      helping you develop, implement, and manage your risk management programs
+      so you can focus on your business in safer, more secure environments.
     </p>
   </Layout>
 )
